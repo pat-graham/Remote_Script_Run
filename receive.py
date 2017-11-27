4861391f9a4626a735a35d7677517736e4a0c20d
Pat <pdgra1@student.monash.edu>
2017-11-27 03:39:29 +0000

    Updated Receive Script to Run File

--- receive.py
+++ receive.py
@@ -10,4 +10,6 @@ sock.bind((UDP_IP, UDP_PORT))
 while True:
 	data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
 	print "received message:", data
-	python berryIMU.py
+	break
+
+import berryIMU
