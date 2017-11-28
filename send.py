4c9cf21fa2557a325643b5e64715f1ea71d827ce
Pat <pdgra1@student.monash.edu>
2017-11-28 23:47:06 +1100

    Send to two sub-Pis

--- send.py
+++ send.py
@@ -1,9 +1,20 @@
 import socket
 
-UDP_IP = "10.0.0.7"
+UDP_IP = "10.1.1.200" #destination IP Address
 UDP_PORT = 5005
 
 MESSAGE = "Starting Recording!"
+
+print "UDP target IP:", UDP_IP
+print "UDP target port:", UDP_PORT
+print "message:", MESSAGE
+
+sock = socket.socket(socket.AF_INET,  # Internet
+                     socket.SOCK_DGRAM)  # UDP
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+UDP_IP = "10.1.1.101" #destination IP Address
+UDP_PORT = 5005
 
 print "UDP target IP:", UDP_IP
 print "UDP target port:", UDP_PORT
